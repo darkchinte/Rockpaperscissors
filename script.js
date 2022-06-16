2748b9f683e8c7188a04f470bee808360e63af97
timex <darkchinte2@gmail.com>
2022-06-17 02:05:25 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -5,10 +5,34 @@ function computerPlay () {
     let random = Math.floor(Math.random() * 3);
 
     if (random <= 0) {
-        return console.log("Paper");
+        return "rock";
     } else if (random == 1) {
-        return console.log("Paper");
+        return "paper";
     } else {
-        return console.log("Scissors");
+        return "scissors";
     }
 }
+
+function playRound (playerSelection, computerSelection) {
+
+   if (playerSelection == 'rock' && computerSelection == 'paper') {
+    return "You lose! rock is absorbed by the paper!";
+   } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return "You win! rock can break the scissors!";
+   } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    return "You lost! paper is weak against scissors!";
+   } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return "You win! paper absorbed the rock!";
+   } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+    return "You lost! scissors do nothing against rocks";
+   } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    return "You win! scissors cutted the paper";
+   } else {
+    return "Its a draw";
+   }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
