fe1dd727abbcb0e5fff2ba5c81824de5be4477f9
timex <darkchinte2@gmail.com>
2022-08-07 01:25:16 +0800

    Implement the functions to the UI

--- script.js
+++ script.js
@@ -19,33 +19,104 @@ function playRound (playerSelection, computerSelection) {
     return 0;
    } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
     return 1;
-   } else {
+   } else if (playerSelection == 'rock' && computerSelection == 'rock' || playerSelection == 'paper' && computerSelection == 'paper' || playerSelection == 'scissors' && computerSelection == 'scissors') {
     return 2;
    }
 
 }
 
 function game() {
+    const result = document.querySelector('.result');
+    const rock = document.getElementById('rock');
+    const paper = document.getElementById('paper');
+    const playerScore = document.querySelector('[data-user="player"]');
+    const computerScore = document.querySelector('[data-user="computer"]');
+    playerScore.textContent = '0';
+    computerScore.textContent = '0';
+    result.textContent = 'THE BATTLE BEGINS!';
     let pScore = 0, cScore = 0;
-    for (let i = 0; i <= Infinity; i++) {
-        // Choosen weapon
-        computerSelection = computerPlay();
-        playerSelection = prompt("Choose: Rock, Paper, Scissors", "");
-        round = playRound(playerSelection, computerSelection);
 
-        // Round winner evaluator 
+    rock.addEventListener('click', () => {
+        round = playRound('rock', computerPlay());
         if (round == 0) {
-            console.log("You Lose!");
+            result.textContent = 'You lose!';
             cScore++;
         } else if (round == 1) {
-            console.log("You Win!");
+            result.textContent = 'You win!';
             pScore++;
-        } else {
-            console.log("It's a Draw");
-        }    
-        console.log(`Your current score is ${pScore} and enemy score is ${cScore}`);
+        } else if (round == 2) {
+            result.textContent = 'It\'s a draw!';
+        }
+        playerScore.textContent = `${pScore}`;
+        computerScore.textContent = `${cScore}`;
 
-        // break the loop if there is a WINNER
-        if (pScore === 5 || cScore === 5) break;
-    }
+        // Stops the function when winner score is reached
+        if (pScore === 5) {
+            playGame.textContent = 'PlAY AGAIN!';
+            result.textContent = 'YOU WON THE BATTLE!!!';
+            return;
+        } else if (cScore === 5) {
+            playGame.textContent = 'PlAY AGAIN!';
+            result.textContent = 'YOU LOST THE BATTLE!!!';
+            return;
+        }
+    })
+
+    paper.addEventListener('click', () => {
+        round = playRound('paper', computerPlay());
+        if (round == 0) {
+            result.textContent = 'You lose!';
+            cScore++;
+        } else if (round == 1) {
+            result.textContent = 'You win!';
+            pScore++;
+        } else if (round == 2) {
+            result.textContent = 'It\'s a draw!';
+        }
+        playerScore.textContent = `${pScore}`;
+        computerScore.textContent = `${cScore}`;
+
+        // Stops the function when winner score is reached
+        if (pScore === 5) {
+            playGame.textContent = 'PlAY AGAIN!';
+            result.textContent = 'YOU WON THE BATTLE!!!';
+            return;
+        } else if (cScore === 5) {
+            playGame.textContent = 'PlAY AGAIN!';
+            result.textContent = 'YOU LOST THE BATTLE!!!';
+            return;
+        }
+    })
+
+    scissors.addEventListener('click', () => {
+        round = playRound('scissors', computerPlay());
+        if (round == 0) {
+            result.textContent = 'You lose!';
+            cScore++;
+        } else if (round == 1) {
+            result.textContent = 'You win!';
+            pScore++;
+        } else if (round == 2) {
+            result.textContent = 'It\'s a draw!';
+        }
+        playerScore.textContent = `${pScore}`;
+        computerScore.textContent = `${cScore}`;
+
+        // Stops the function when winner score is reached
+        if (pScore === 5) {
+            playGame.textContent = 'PlAY AGAIN!';
+            result.textContent = 'YOU WON THE BATTLE!!!';
+            return;
+        } else if (cScore === 5) {
+            playGame.textContent = 'PlAY AGAIN!';
+            result.textContent = 'YOU LOST THE BATTLE!!!';
+            return;
+        }
+    })
+
+    
 }
+
+
+const playGame = document.getElementById('play');
+playGame.addEventListener('click', game)
